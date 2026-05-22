c0bc0b6a334deefb522dd2bbb05b822478e89df1
aina <azidyaina615@gmail.com>
2026-05-22 14:44:02 +0800

    first commit

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+/**
+ * Welcome to Cloudflare Workers! This is your first worker.
+ *
+ * - Run `npm run dev` in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run `npm run deploy` to publish your worker
+ *
+ * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
+ * `Env` object can be regenerated with `npm run cf-typegen`.
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
+
+// export default {
+// 	async fetch(request, env, ctx): Promise<Response> {
+// 		return new Response("Hello World!");
+// 	},
+// } satisfies ExportedHandler<Env>;
+
+export default {
+	async fetch(request, env) {
+		const url = new URL(request.url);
+		if (url.pathname === "/api/config") {
+			return Response.json({
+				greeting: env.GREETING,
+				hasKey: !!env.API_KEY,
+			});
+		}
+		return env.ASSETS.fetch(request);
+	},
+} satisfies ExportedHandler<Env>;
